e2a7d22af1da5860feb66c06e13714a8253f92d6
kalravm41 <64910029+kalravm41@users.noreply.github.com>
2021-08-13 21:57:40 +0530

    Add files via upload

---
+++ ielts mock/ielts mock-1/logic.js
@@ -0,0 +1,25 @@
+var audio1 = document.getElementById('aud1');
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        timer= 00;
+        window.location.replace('./reading.html');
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var fiveMinutes = 60 * 5,
+      display = document.querySelector('#time');
+  startTimer(2400, display);
+};
